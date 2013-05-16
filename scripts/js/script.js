2d02850845f33e026a95415a328c9881481b9d12
halebw <halebenw@gmail.com>
2013-05-16 09:36:23 -0400

    commit to build app

--- scripts/js/script.js
+++ scripts/js/script.js
@@ -1,13 +1,23 @@
-$(document).ready(function() {
 
-	$("nav p").click(function(e) {
-		var linkName = $(this).html().toLowerCase();
-		var url = "scripts/php/func.php";
-		var posting = $.post(url, {func: "nav", navName: linkName});
-		
-		posting.done(function(data) {
-		$('#content').html(data);
+$(document).ready(function(){
+
+
+$("nav p").click(function(e){
+var linkName = $(this).html().toLowerCase();
+var url = "scripts/php/func.php";
+
+var posting = $.post(url, {func: "nav", navName: linkName});
+
+
+posting.fail(function(e){
+$.get("content/" + linkName + ".html", function(data){
+$('#content').html(data);
+});
 });
 
-	});
+posting.done(function(data) {
+$('#content').html(data);
+});
+
+});
 });
